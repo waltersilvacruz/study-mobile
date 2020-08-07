2d900f3af357d6244884f2f153fc55844b263671
Walter Cruz <walter@tnx.com.br>
2020-08-07 09:38:23 -0400

    PageHeader component

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { View } from 'react-native';
 
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
 
 function Favorites() {
-    return <View />;
+    return (
+        <View style={styles.container}>
+            <PageHeader title="Meus Proffys favoritos" />
+        </View>
+    );
 }
 
 export default Favorites;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { View } from 'react-native';
 
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
 
 function TeacherList() {
-    return <View />;
+    return (
+        <View style={styles.container}>
+            <PageHeader title="Proffys dispiníveis" />
+        </View>
+    );
 }
 
 export default TeacherList;
