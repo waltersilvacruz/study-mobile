45d47a162e0f970f80eca4d3428fac9b7a7592f7
Walter Cruz <walter@tnx.com.br>
2020-09-11 12:06:23 -0400

    Ajustes

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
@@ -8,6 +8,12 @@ function TeacherList() {
     return (
         <View style={styles.container}>
             <PageHeader title="Proffys dispiníveis" />
+            <View style={styles.listContainer}>
+                <View style={styles.listItem}>
+                    
+                    <Text>Walter Cruz</Text>
+                </View>
+            </View>
         </View>
     );
 }
--- src/pages/TeacherList/styles.ts
+++ src/pages/TeacherList/styles.ts
@@ -5,6 +5,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#f0f0f7'
+    },
+
+    listContainer: {
+        marginTop: -40,
+        flex: 1,
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+
+    listItem: {
+        width: '92%',
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        padding: 20
     }
 });
 
